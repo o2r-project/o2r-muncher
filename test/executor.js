a8e0884012b6174f55d69b7eb7cb66b045e95401
Jan Koppe <j_kopp07@uni-muenster.de>
2016-04-06 11:00:01 +0200

    fixed statuscode detection

--- test/executor.js
+++ test/executor.js
@@ -4,9 +4,14 @@ var Executor = require('../lib/executor').Executor;
 
 var execution = new Executor('test', '../workspace/').execute();
 
-execution.catch(e => { 
+execution.catch(e => {
   debug('failure: ' + e);
 });
 execution.then(() => {
   debug('success!');
+});
+
+process.on('unhandledRejection', function(reason, p){
+    console.log("Possibly Unhandled Rejection at: Promise ", p, " reason: ", reason);
+    // application specific logging here
 });
