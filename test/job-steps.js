1f3a7140f85b43417016b9406218489a43c01a42
nuest <daniel.nuest@uni-muenster.de>
2016-11-18 15:37:45 +0100

    extract util funct. for tests; add license header

--- test/job-steps.js
+++ test/job-steps.js
@@ -1,11 +1,27 @@
+/*
+ * (C) Copyright 2016 o2r project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 /* eslint-env mocha */
 const assert = require('chai').assert;
 const request = require('request');
 const config = require('../config/config');
+const createCompendiumPostRequest = require('./util').createCompendiumPostRequest;
 const host = 'http://localhost:' + config.net.port;
-const AdmZip = require('adm-zip');
 const fs = require('fs');
-const tmp = require('tmp');
 const sleep = require('sleep');
 var unamecall = require('node-uname');
 
@@ -13,38 +29,6 @@ require("./setup")
 const cookie_o2r = 's:C0LIrsxGtHOGHld8Nv2jedjL4evGgEHo.GMsWD5Vveq0vBt7/4rGeoH5Xx7Dd2pgZR9DvhKCyDTY';
 const cookie_plain = 's:yleQfdYnkh-sbj9Ez--_TWHVhXeXNEgq.qRmINNdkRuJ+iHGg5woRa9ydziuJ+DzFG9GnAZRvaaM';
 const sleepSecs = 1;
-
-function createCompendiumPostRequest(path, cookie) {
-  var zip = new AdmZip();
-  zip.addLocalFolder(path);
-  var tmpfile = tmp.tmpNameSync() + '.zip';
-  //var zipBuffer = zip.toBuffer(); could not make buffer work with multipart/form
-  zip.writeZip(tmpfile);
-
-  let formData = {
-    'content_type': 'compendium_v1',
-    'compendium': {
-      value: fs.createReadStream(tmpfile),
-      options: {
-        filename: 'another.zip',
-        contentType: 'application/zip'
-      }
-    }
-  };
-  let j = request.jar();
-  let ck = request.cookie('connect.sid=' + cookie);
-  j.setCookie(ck, host);
-
-  let reqParams = {
-    uri: host + '/api/v1/compendium',
-    method: 'POST',
-    jar: j,
-    formData: formData,
-    timeout: 1000
-  };
-
-  return (reqParams);
-}
 
 describe('API Job', () => {
   describe('GET /api/v1/job (with no job started)', () => {
@@ -77,7 +61,7 @@ describe('API Job', () => {
     let job_id = '';
 
     it('upload compendium should succeed and return an ID', (done) => {
-      let req = createCompendiumPostRequest('./test/bagtainers/step_zero', cookie_o2r);
+      let req = createCompendiumPostRequest(host, './test/bagtainers/step_zero', cookie_o2r);
 
       request(req, (err, res, body) => {
         assert.ifError(err);
@@ -194,7 +178,7 @@ describe('API Job', () => {
     let job_id = '';
 
     it('upload compendium should succeed and return an ID', (done) => {
-      let req = createCompendiumPostRequest('./test/bagtainers/step_validate_bag', cookie_o2r);
+      let req = createCompendiumPostRequest(host, './test/bagtainers/step_validate_bag', cookie_o2r);
       // useful command: unzip -l /tmp/tmp-5697QCBn11BrFvTl.zip 
 
       request(req, (err, res, body) => {
@@ -293,7 +277,7 @@ describe('API Job', () => {
     let job_id = '';
 
     it('upload compendium should succeed and return an ID', (done) => {
-      let req = createCompendiumPostRequest('./test/bagtainers/step_validate_compendium', cookie_o2r);
+      let req = createCompendiumPostRequest(host, './test/bagtainers/step_validate_compendium', cookie_o2r);
       // useful command: unzip -l /tmp/tmp-5697QCBn11BrFvTl.zip 
 
       request(req, (err, res, body) => {
@@ -440,7 +424,7 @@ describe('API Job', () => {
     let job_id = '';
 
     it('upload compendium should succeed and return an ID', (done) => {
-      let req = createCompendiumPostRequest('./test/bagtainers/step_image_prepare', cookie_o2r);
+      let req = createCompendiumPostRequest(host, './test/bagtainers/step_image_prepare', cookie_o2r);
       // useful command: unzip -l /tmp/tmp-5697QCBn11BrFvTl.zip 
 
       request(req, (err, res, body) => {
@@ -530,7 +514,7 @@ describe('API Job', () => {
     var job_id = '';
 
     it('upload compendium should succeed and return an ID', (done) => {
-      let req = createCompendiumPostRequest('./test/bagtainers/step_image_build', cookie_o2r);
+      let req = createCompendiumPostRequest(host, './test/bagtainers/step_image_build', cookie_o2r);
       // useful command: unzip -l /tmp/tmp-5697QCBn11BrFvTl.zip 
 
       request(req, (err, res, body) => {
@@ -617,7 +601,7 @@ describe('API Job', () => {
     var docker = new Docker();
 
     it('upload compendium should succeed and return an ID', (done) => {
-      let req = createCompendiumPostRequest('./test/bagtainers/step_image_execute', cookie_o2r);
+      let req = createCompendiumPostRequest(host, './test/bagtainers/step_image_execute', cookie_o2r);
 
       request(req, (err, res, body) => {
         assert.ifError(err);
