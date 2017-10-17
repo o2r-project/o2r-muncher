caaf177568178339f4434a3c1e1c9eb28a8c7a58
nuest <daniel.nuest@uni-muenster.de>
2017-10-17 14:57:13 +0200

    fix copyright year

--- test/meta.js
+++ test/meta.js
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2016 o2r project
+ * (C) Copyright 2017 o2r project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
