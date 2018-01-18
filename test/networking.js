8289ac6d0006c8a8cc1a83add5fec909aa41e0c9
nuest <daniel.nuest@uni-muenster.de>
2018-01-18 10:51:06 +0100

    run all tests

--- test/networking.js
+++ test/networking.js
@@ -33,7 +33,7 @@ const cookie_editor = 's:xWHihqZq6jEAObwbfowO5IwdnBxohM7z.VxqsRC5A1VqJVspChcxVPu
 
 const sleepSecs = 15;
 
-describe.('Container networking', () => {
+describe('Container networking', () => {
   var db = mongojs('localhost/muncher', ['compendia', 'jobs']);
 
   after(function (done) {
